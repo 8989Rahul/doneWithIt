1e9790059ada1a02ea44cb86e82c63b5d6d291f4
8989Rahul <daghalerahul0@gmail.com>
2020-07-28 10:28:16 +0530

    welcome screen basic ui done

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import WelcomScreen from "./App/screens/WelcomScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Rahul , hey how are you?</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return  <WelcomScreen />
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
